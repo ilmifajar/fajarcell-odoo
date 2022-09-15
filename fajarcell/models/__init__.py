47b49ded3fce56144ffe18f66298d6a1111822da
ilmifajar <102019510+ilmifajar@users.noreply.github.com>
2022-09-15 10:40:47 +0700

    Add files via upload

---
+++ fajarcell/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+from . import produk
+from . import operatorproduk
+from . import person
+from . import konsumen
+from . import ResPartner
+from . import penjualan
+from . import provider
