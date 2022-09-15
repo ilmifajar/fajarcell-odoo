7afe6fe2774fdc01449bdad5bf94c916fa93ad3d
ilmifajar <102019510+ilmifajar@users.noreply.github.com>
2022-09-15 10:25:03 +0700

    Delete fajarcell directory

--- fajarcell/models/ResPartner.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-
-class ResParner(models.Model):
-    _inherit = 'res.partner'
-    _description = 'New Description'
-
-    is_konsumen = fields.Boolean(string='is konsumen')
-    is_direksi = fields.Boolean(string='is direksi')
