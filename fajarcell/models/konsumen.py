f03d242a592af157f5549931f86bceb8608164ec
ilmifajar <102019510+ilmifajar@users.noreply.github.com>
2022-09-15 08:07:00 +0700

    Delete fajarcell directory

--- fajarcell/models/konsumen.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-
-class Konsumen(models.Model):
-    _inherit = 'res.partner'
-    _description = 'Contact'
-
-    poin = fields.Integer(string='Poin')
-    level = fields.Char(string='Level')
